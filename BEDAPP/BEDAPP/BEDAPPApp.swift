981364f104340dc4c62a15d5885969cc75b7896c
Puggio77 <147837226+Puggio77@users.noreply.github.com>
2026-01-23 11:13:33 +0100

    initial commit

---
+++ BEDAPP/BEDAPP/BEDAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  BEDAPPApp.swift
+//  BEDAPP
+//
+//  Created by Riccardo Puggioni on 23/01/26.
+//
+
+import SwiftUI
+
+@main
+struct BEDAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
